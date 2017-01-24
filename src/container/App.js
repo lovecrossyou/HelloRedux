319c133e1689081d16d3bb68f3ab39bc682ffa27
lovecross <lovecrossyoutt@hotmail.com>
2017-01-24 23:33:49 +0800

    。。。。。

---
+++ src/container/App.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zhulizhe on 2017/1/24.
+ */
+import React from 'react'
+import AddTodo from './AddTodo'
+
+const App = () => (
+    <div>
+        <AddTodo />
+    </div>
+)
+
+export default App
